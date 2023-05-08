6d39a02bbceed978ed46c4317b17699fe50953a2
OscarBaMo <127763844+OscarBaMo@users.noreply.github.com>
2023-05-08 08:36:42 -0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.net.http.WebSocket;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Lista list = new Lista();
+        String dato = "";
+        Scanner sc = new Scanner(System.in);
+        while (!dato.equalsIgnoreCase("fin")){
+            dato = sc.nextLine();
+        if (!dato.equalsIgnoreCase("fin"))
+            list.insertar(dato);
+        }
+        list.visualizar();
+    }
+}
